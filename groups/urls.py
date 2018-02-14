b3890d9a43c410fa4fa9f3c590daaac3584f1a81
Ou Jingzhou <jingzhou1994@gmail.com>
2018-02-14 14:43:20 +0200

    convert groupeditview into class based

--- groups/urls.py
+++ groups/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
-from groups.views import UserGroupListView, UserGroupDeleteView, edit_group
+from groups.views import UserGroupListView, UserGroupDeleteView, UserGroupEditView
 
 urlpatterns = [
     url(r'^$', UserGroupListView.as_view(), name='usergroup-list'),
-    url(r'^new/$', edit_group, name='group-edit'),
-    url(r'^([\w\s-]+)/$', edit_group, name='group-edit'),
+    url(r'^new/$', UserGroupEditView.as_view(), name='group-edit'),
+    url(r'^(?P<group_name>[\w\s-]+)/$', UserGroupEditView.as_view(), name='group-edit'),
     url(r'^delete/(?P<group_name>[\w\s-]+)/$', UserGroupDeleteView.as_view(), name='group-delete')
 
 ]
--- groups/views.py
+++ groups/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DeleteView
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
 from .models import UserProfile, UserGroup, Membership
 from .forms import UserGroupForm
@@ -26,34 +27,49 @@ class UserGroupDeleteView(DeleteView):
             usergroup = None
         return usergroup
 
+class UserGroupEditView(FormView):
+    template_name = 'groups/edit.html'
+    form_class = UserGroupForm
+    success_url = 'usergroup-list'
 
-def edit_group(request, group_name=None):
-    if group_name:
-        usergroup = get_object_or_404(UserGroup, group_name=group_name)
-        form = UserGroupForm(request.POST or None, instance=usergroup)
-        form.fields['administrators'].queryset = UserProfile.objects.all()
-        form.fields['administrators'].initial = get_administrators(usergroup)
-        form.fields['parent'].queryset = UserGroup.objects.all().exclude(group_name = group_name)
-    else:
-        usergroup = None
-        form = UserGroupForm(request.POST or None)
-    for name in form.fields:
-        form.fields[name].widget.attrs = {'class': 'form-control'}
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('usergroup-list')
-    return render(request, 'groups/edit.html', {
-        'usergroup': usergroup,
-        'form': form,
-    })
+    def get_context_data(self, **kwargs):
+        group_name = ''
+        if 'group_name' in kwargs:
+            group_name = self.kwargs['group_name']
+        if group_name:
+            usergroup = get_object_or_404(UserGroup, group_name=group_name)
+            form = UserGroupForm(self.request.POST or None, instance=usergroup)
+            form.fields['administrators'].queryset = UserProfile.objects.all()
+            form.fields['administrators'].initial = self.get_administrators(usergroup)
+            form.fields['parent'].queryset = UserGroup.objects.all().exclude(group_name=group_name)
+        else:
+            usergroup = None
+            form = UserGroupForm(self.request.POST or None)
+        for name in form.fields:
+            form.fields[name].widget.attrs = {'class': 'form-control'}
+        context = {
+                'usergroup': usergroup,
+                'form': form,
+            }
+        return context
 
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return render(request, self.template_name, context)
 
-def get_administrators(usergroup):
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        form = context['form']
+        if form.is_valid():
+            form.save()
+            return redirect(self.success_url)
+        return render(request, self.template_name, context)
 
-    current_administrators_memberships = Membership.objects.filter(usergroup=usergroup, is_administrator=True)
-    current_administrators_id = []
-    for current_administrators_membership in current_administrators_memberships:
-        administrator_id = current_administrators_membership.userprofile.id
-        current_administrators_id.append(administrator_id)
-
-    return UserProfile.objects.filter(id__in=current_administrators_id)
+    @staticmethod
+    def get_administrators(usergroup):
+        current_administrators_memberships = Membership.objects.filter(usergroup=usergroup, is_administrator=True)
+        current_administrators_id = []
+        for current_administrators_membership in current_administrators_memberships:
+            administrator_id = current_administrators_membership.userprofile.id
+            current_administrators_id.append(administrator_id)
+        return UserProfile.objects.filter(id__in=current_administrators_id)
